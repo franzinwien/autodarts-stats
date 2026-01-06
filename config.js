19c641366d51ead083c305268206af5130a97f72
franzinwien <lusser@gmail.com>
2026-01-06 10:29:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // Supabase Configuration
-    SUPABASE_URL: 'https://jumwvrxugaeruahjsset.supabase.co',
+    SUPABASE_URL: 'https://junwvrxugaeruahjsset.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1bnd2cnh1Z2FlcnVhaGpzc2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1Mzg2MDEsImV4cCI6MjA4MTExNDYwMX0.-0R0U0uKVRs6-Sov8tv80ip7fjkFVAHIngOVlWnjoNQ',
     
     // App Settings
